bf1b856f76c0f47b8394e134b839bf543cc8479c
Ruslan Dzhamaev <ruslan.dzhamaev@gmail.com>
2016-06-28 14:35:38 +0300

    Fix headers

--- app/src/main/java/org/rudzha/flickrdisplayer/adapters/viewholders/AbsViewHolder.java
+++ app/src/main/java/org/rudzha/flickrdisplayer/adapters/viewholders/AbsViewHolder.java
@@ -4,9 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 /**
- * Copyright (c) 2016 PIQ. All rights reserved.
- * <p/>
- * Created by ruslandzhamaev on  22.06.16.
+ * Base viewholder class that promotes binding logic delegation
  */
 public abstract class AbsViewHolder<T> extends RecyclerView.ViewHolder {
     public AbsViewHolder(View itemView) {
